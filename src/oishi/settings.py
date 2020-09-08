6a6ccf24361a6069f1423ae22e1d75a00c74099e
edamame2135 <46081659+edamame2135@users.noreply.github.com>
2020-09-07 18:53:46 -0700

    models

--- src/oishi/settings.py
+++ src/oishi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'vd)gy2b$p*fhba=ayze%*0(bz$xwu6_e4wp*pal_*dej2h$xm*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #own
+    'products',
 ]
 
 MIDDLEWARE = [
